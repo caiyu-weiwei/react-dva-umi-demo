3db7d60466e1f4c022445292728c82052b208dc4
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2019-07-04 14:06:36 +0800

    添加user页面

--- .umirc.js
+++ .umirc.js
@@ -22,4 +22,13 @@ export default {
       },
     }],
   ],
+  proxy: {
+    '/api':{
+      target: 'http://jsonplaceholder.typicode.com/',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': ''
+      }
+    }
+  }
 }
---
+++ src/pages/users/index.js
@@ -0,0 +1,5 @@
+export default () => {
+  return (
+    <div>User Page</div>
+  )
+}
